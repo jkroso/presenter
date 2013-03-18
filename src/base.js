61177952350a34cd7e7ffeefbe9f3c2baf5c0d73
jkroso <jkroso@gmail.com>
2013-03-18 13:58:58 +1300

    update api

---
+++ src/base.js
@@ -0,0 +1,158 @@
+
+var DomEmitter = require('dom-emitter')
+  , ChildList = require('./childlist')
+  , classlist = require('classes')
+  , domify = require('domify')
+  , matches = require('matches-selector')
+
+module.exports = Presenter
+
+/**
+ * Presenter
+ * 
+ * If `view` is a string it will 
+ * be converted to a HTML DOM element
+ *
+ * @param {String|Element} view
+ */
+
+function Presenter (view) {
+  if (typeof view == 'string') view = domify(view)
+  this.view = view
+  this.kids =
+  this.children = new ChildList(view, this)
+  this.classList = classlist(view)
+  this.events = new DomEmitter(view, this)
+}
+
+/**
+ * Insert `this` as the `prevSibling` of `sib`
+ *
+ * @param {Presenter} sib
+ */
+
+Presenter.prototype.insertBefore = function (sib) {
+  this.parent = sib.parent
+  var prev = sib.prevSibling
+  if (prev) prev.nextSibling = this
+  sib.prevSibling = this
+  this.nextSibling = sib
+  this.prevSibling = prev
+
+  sib.view.parentNode.insertBefore(this.view, sib.view)
+}
+
+/**
+ * Insert `this` as the `nextSibling` of `sib`
+ * 
+ * @param {Presenter} sib
+ */
+
+Presenter.prototype.insertAfter = function (sib) {
+  this.parent = sib.parent
+
+  var next = sib.nextSibling
+  this.nextSibling = next
+  if (next) next.prevSibling = this
+  sib.nextSibling = this
+  this.prevSibling = sib
+
+  sib.view.parentNode.insertBefore(this.view, sib.view.nextSibling)
+}
+
+/**
+ * Get a list of siblings in DOM order
+ *
+ * @param {Boolean} inc to include `this` in the list
+ * @return {Array}
+ */
+
+Presenter.prototype.siblings = function (inc) {
+  var sibs = prevSibs(this.prevSibling)
+  inc && sibs.push(this)
+  var sib = this.nextSibling
+  while (sib) {
+    sibs.push(sib)
+    sib = sib.nextSibling
+  }
+  return sibs
+}
+
+function prevSibs (view) {
+  if (!view) return []
+  var prev = prevSibs(view.prevSibling)
+  prev.push(view)
+  return prev
+}
+
+/**
+ * Remove a view from the DOM
+ * 
+ * @emits "remove"
+ */
+ 
+Presenter.prototype.remove = function () {
+  var parent = this.view.parentNode
+  if (parent) {
+    this.events.emit('remove')
+    parent.removeChild(this.view)
+  }
+}
+
+/**
+ * Find the first parent matching `sel`
+ *
+ * @param {String} sel
+ * @return {Presenter}
+ */
+
+Presenter.prototype.up = function(sel){
+  var parent = this
+  while (parent = parent.parent) {
+    if (matches(parent.view, sel)) return parent
+  }
+}
+
+/**
+ * Find the closest child matching `sel`.
+ * The search is run breadth first.
+ *
+ * @param {String} sel
+ * @return {Presenter}
+ */
+
+Presenter.prototype.down = function(sel){
+  var childs = this.children.toArray()
+  for (var i = 0; i < childs.length; i++) {
+    var child = childs[i]
+    if (matches(child.view, sel)) return child
+    child.children.each(push)
+  }
+
+  function push(child){
+    childs.push(child)
+  }
+}
+
+/**
+ * Works like `.down` except later siblings are prefered
+ *
+ * @param {String} sel
+ * @return {Presenter}
+ */
+
+Presenter.prototype.downLast = function(sel){
+  var childs = this.children.toArray().reverse()
+  for (var i = 0; i < childs.length; i++) {
+    var child = childs[i]
+    if (matches(child.view, sel)) return child
+    reversePush(child.children.last)
+  }
+
+  function reversePush(child){
+    while (child) {
+      childs.push(child)
+      child = child.prevSibling
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,167 +1,69 @@
 
-var DomEmitter = require('dom-emitter')
+var Base = require('./base')
   , ChildList = require('./childlist')
-  , classlist = require('classes')
-  , domify = require('domify')
-  , matches = require('matches-selector')
+  , graph = require('graph')
 
-module.exports = Presenter
-Presenter.ChildList = ChildList
+module.exports = makePresenter
+makePresenter.ChildList = ChildList
+makePresenter.Base = Base
+
 
 /**
- * Presenter constructor. 
+ * Create a Presenter factory
+ * 
  * If you pass a String as `view` it will be converted to 
  * a DOM element. If you pass a function it will be called 
  * with `model.toJSON()` to produce a string which gets 
  * converted to a DOM element.
  *
- * @param {String|DomElement} view
- * @param {Any} model
+ * @param {String} [name]
+ * @param {Function|String} template
+ * @return {Function}
  */
 
-function Presenter (view, model) {
-  if (typeof view === 'function') {
-    view = view(model && model.toJSON ? model.toJSON() : model)
-  }
-  if (typeof view === 'string') {
-    view = domify(view)
-  }
-  this.view = view
-  this.model = model
-  this.kids =
-  this.children = new ChildList(view, this)
-  this.classList = classlist(view)
-  this.events = new DomEmitter(view, this)
+function makePresenter(name, template){
+	if (arguments.length < 2) {
+		template = name
+		name = 'anonymous_presenter'
+	}
+
+	// compile constructor
+	var Pres = eval(
+		'(function '+name+'(model){\n' +
+		'  this.model = model\n' +
+		(typeof template == 'string' 
+			? '  Base.call(this, template)'
+			: '  Base.call(this, template(model && model.toJSON ? model.toJSON() : model))'
+		) + '\n' +
+		'  var behaviour = '+name+'.behaviour\n' +
+		'  for (var i = 0, len = behaviour.length; i < len; i++) {\n' +
+		'    var b = behaviour[i]\n' +
+		'    var act = b.action\n' +
+		'    if (typeof act == "object") act = graph(act)\n' +
+		'    this.events.on(b.trigger, act)\n' +
+		'  }\n' +
+		'  if (typeof this.init == "function") this.init(model)\n' +
+		'})'
+	)
+
+	Pres.behaviour = []
+
+	Pres.behave = addBehaviour
+
+	// inherit Base
+	Pres.prototype.__proto__ = Base.prototype
+
+	return Pres
 }
 
-/**
- * Insert `this` as the `prevSibling` of `sib`
- *
- * @param {Presenter} sib
- */
-
-Presenter.prototype.insertBefore = function (sib) {
-  this.parent = sib.parent
-  var prev = sib.prevSibling
-  if (prev) prev.nextSibling = this
-  sib.prevSibling = this
-  this.nextSibling = sib
-  this.prevSibling = prev
-
-  sib.view.parentNode.insertBefore(this.view, sib.view)
-}
-
-/**
- * Insert `this` as the `nextSibling` of `sib`
- * 
- * @param {Presenter} sib
- */
-
-Presenter.prototype.insertAfter = function (sib) {
-  this.parent = sib.parent
-
-  var next = sib.nextSibling
-  this.nextSibling = next
-  if (next) next.prevSibling = this
-  sib.nextSibling = this
-  this.prevSibling = sib
-
-  sib.view.parentNode.insertBefore(this.view, sib.view.nextSibling)
-}
-
-/**
- * Get a list of siblings in DOM order
- *
- * @param {Boolean} inc to include `this` in the list
- * @return {Array}
- */
-
-Presenter.prototype.siblings = function (inc) {
-  var sibs = prevSibs(this.prevSibling)
-  inc && sibs.push(this)
-  var sib = this.nextSibling
-  while (sib) {
-    sibs.push(sib)
-    sib = sib.nextSibling
-  }
-  return sibs
-}
-
-function prevSibs (view) {
-  if (!view) return []
-  var prev = prevSibs(view.prevSibling)
-  prev.push(view)
-  return prev
-}
-
-/**
- * Remove a view from the DOM
- * 
- * @emits "remove"
- */
- 
-Presenter.prototype.remove = function () {
-  var parent = this.view.parentNode
-  if (parent) {
-    this.events.emit('remove')
-    parent.removeChild(this.view)
-  }
-}
-
-/**
- * Find the first parent matching `sel`
- *
- * @param {String} sel
- * @return {Presenter}
- */
-
-Presenter.prototype.up = function(sel){
-  var parent = this
-  while (parent = parent.parent) {
-    if (matches(parent.view, sel)) return parent
-  }
-}
-
-/**
- * Find the closest child matching `sel`.
- * The search is run breadth first.
- *
- * @param {String} sel
- * @return {Presenter}
- */
-
-Presenter.prototype.down = function(sel){
-  var childs = this.children.toArray()
-  for (var i = 0; i < childs.length; i++) {
-    var child = childs[i]
-    if (matches(child.view, sel)) return child
-    child.children.each(push)
-  }
-
-  function push(child){
-    childs.push(child)
-  }
-}
-
-/**
- * Works like `.down` except later siblings are prefered
- *
- * @param {String} sel
- * @return {Presenter}
- */
-
-Presenter.prototype.downLast = function(sel){
-  var childs = this.children.toArray().reverse()
-  for (var i = 0; i < childs.length; i++) {
-    var child = childs[i]
-    if (matches(child.view, sel)) return child
-    reversePush(child.children.last)
-  }
-
-  function reversePush(child){
-    while (child) {
-      childs.push(child)
-      child = child.prevSibling
-    }
-  }
+function addBehaviour(trigger, action){
+	if (typeof trigger == 'function') {
+		action = trigger
+		trigger = trigger.name
+	}
+	this.behaviour.push({
+		trigger: trigger,
+		action: action
+	})
+	return this
 }
